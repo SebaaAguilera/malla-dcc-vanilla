94fd59373219ed4518159072e16b57365f31e4cc
Eric K <54124297+Nyveon@users.noreply.github.com>
2022-08-20 00:23:52 -0400

    lints main script

--- public/script.js
+++ public/script.js
@@ -1,90 +1,94 @@
 const createElement = (tag, attributes, ...children) => {
-  const element = document.createElement(tag)
-  Object.entries(attributes).forEach(([key, attr]) => element.setAttribute(key, attr))
-  children.forEach(child => element.appendChild(child))
-  return element
-}
+  const element = document.createElement(tag);
+  Object.entries(attributes).forEach(([key, attr]) =>
+    element.setAttribute(key, attr));
+  children.forEach((child) => element.appendChild(child));
+  return element;
+};
 
 const changeStyleById = (id, status) => {
-  const elementStyle = document.getElementById(id).style
+  const elementStyle = document.getElementById(id).style;
   switch (status) {
     case 'required':
-      elementStyle.backgroundColor = 'rgba(147,197,253,0.5)'
-      elementStyle.border = '2px solid rgb(147,197,253)'
+      elementStyle.backgroundColor = 'rgba(147,197,253,0.5)';
+      elementStyle.border = '2px solid rgb(147,197,253)';
       break;
     case 'simultaneous':
-      elementStyle.backgroundColor = 'rgba(255,211,77,0.5)'
-      elementStyle.border = '2px solid rgb(255,211,77)'
+      elementStyle.backgroundColor = 'rgba(255,211,77,0.5)';
+      elementStyle.border = '2px solid rgb(255,211,77)';
       break;
     case 'or':
-      elementStyle.backgroundColor = 'rgba(252,165,165,0.5)'
-      elementStyle.border = '2px solid rgb(252,165,165)'
+      elementStyle.backgroundColor = 'rgba(252,165,165,0.5)';
+      elementStyle.border = '2px solid rgb(252,165,165)';
       break;
     default:
-      elementStyle.backgroundColor = 'lightgrey'
-      elementStyle.border = '2px solid lightgrey'
+      elementStyle.backgroundColor = 'lightgrey';
+      elementStyle.border = '2px solid lightgrey';
       break;
   }
-}
+};
 
 const createSubject = (subject) => {
-  let paragraph = document.createElement('p')
-  paragraph.innerHTML = `${subject.name}<br>${subject.code}`
+  const paragraph = document.createElement('p');
+  paragraph.innerHTML = `${subject.name}<br>${subject.code}`;
 
-  let subjectDiv = createElement('div', { id: subject.code, class: 'subject' }, paragraph)
+  const subjectDiv =
+    createElement('div', {id: subject.code, class: 'subject'}, paragraph);
 
   subjectDiv.onmouseover = () => {
-    subjectDiv.style.backgroundColor = 'rgba(110,231,183,0.5)'
-    subjectDiv.style.border = '2px solid rgb(110,231,183)'
-    subject.requirements.forEach(requirement => {
+    subjectDiv.style.backgroundColor = 'rgba(110,231,183,0.5)';
+    subjectDiv.style.border = '2px solid rgb(110,231,183)';
+    subject.requirements.forEach((requirement) => {
       if (requirement.endsWith('S')) {
-        changeStyleById(requirement.slice(0, -1), 'simultaneous')
+        changeStyleById(requirement.slice(0, -1), 'simultaneous');
       } else if (requirement.indexOf('/') > -1) {
-        requirement.split('/').forEach(requirement => changeStyleById(requirement, 'or'))
+        requirement.split('/').forEach((requirement) =>
+          changeStyleById(requirement, 'or'));
       } else {
-        changeStyleById(requirement, 'required')
+        changeStyleById(requirement, 'required');
       }
-    })
-  }
+    });
+  };
 
   subjectDiv.onmouseout = () => {
-    subjectDiv.style.backgroundColor = 'lightgrey'
-    subjectDiv.style.border = '2px solid lightgrey'
-    subject.requirements.forEach(requirement => {
+    subjectDiv.style.backgroundColor = 'lightgrey';
+    subjectDiv.style.border = '2px solid lightgrey';
+    subject.requirements.forEach((requirement) => {
       if (requirement.endsWith('S')) {
-        changeStyleById(requirement.slice(0, -1))
+        changeStyleById(requirement.slice(0, -1));
       } else if (requirement.indexOf('/') > -1) {
-        requirement.split('/').forEach(requirement => changeStyleById(requirement))
+        requirement.split('/').forEach((requirement) =>
+          changeStyleById(requirement));
       } else {
-        changeStyleById(requirement)
+        changeStyleById(requirement);
       }
-    })
-  }
+    });
+  };
 
-  return subjectDiv
-}
+  return subjectDiv;
+};
 
 const createSemester = (number, subjects) => {
-  let paragraph = document.createElement('p')
-  paragraph.innerHTML = `SEMESTRE <br> ${number}`
+  const paragraph = document.createElement('p');
+  paragraph.innerHTML = `SEMESTRE <br> ${number}`;
 
   return createElement('div', {
     id: `semester-${number}`,
-    class: 'semester'
+    class: 'semester',
   },
-    createElement('div', {
-      class: 'semester-title',
-    },
-      paragraph,
-    ),
-    ...subjects.map(subject => createSubject(subject)),
-  )
-}
+  createElement('div', {
+    class: 'semester-title',
+  },
+  paragraph,
+  ),
+  ...subjects.map((subject) => createSubject(subject)),
+  );
+};
 
 const mount = (malla) => {
-  const canvas = document.getElementById('malla')
-  canvas.innerHTML = ''
-  malla.semesters.map(({ number, subjects }) => {
-    canvas.appendChild(createSemester(number, subjects))
-  })
-}
+  const canvas = document.getElementById('malla');
+  canvas.innerHTML = '';
+  malla.semesters.map(({number, subjects}) => {
+    canvas.appendChild(createSemester(number, subjects));
+  });
+};
